abb6e915df77eda3c3785d0b5262d7cb883e0203
dmytrovasylyna <vasilina.dmitrii@gmail.com>
2025-01-26 18:56:36 +0200

    123

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -11,5 +11,39 @@ public class ASimplePlaywrightTest {
     @Test
     void shouldShowThePageTitle() {
         // TODO: Write your first playwright test here
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void shouldSearchByKeyword () {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        page.locator("//input[@id='search-query']").fill("pliers");
+        page.locator("//button[@type='submit']").click();
+
+        int countOfCardBySearchResult = page.locator("//a[@class='card']").count();
+        System.out.println("countOfCardBySearchResult = " + countOfCardBySearchResult);
+
+        Assertions.assertTrue(countOfCardBySearchResult > 0, "search doesn't have result");
+
+        page.close();
+        browser.close();
+
     }
 }
